538ac81481b1d12a7c626409d873209cdc296fe5
nainaikuo <87597724+nainaikuo@users.noreply.github.com>
2023-12-27 09:48:20 +0800

    js update
    page acticity js update

--- pages/activity_web/js/all.js
+++ pages/activity_web/js/all.js
@@ -3,40 +3,41 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 
 function init() {
     fetch("./data/data.json")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (result) {
-    renderActivity(result.activity)
-    renderPolicy(result.policy)
-    renderDonationProject(result.donation_project)
-  });
-  }
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (result) {
+            renderActivity(result.activity)
+            renderPolicy(result.policy)
+            renderDonationProject(result.donation_project)
+            renderSloganChip(result.slogan)
+        });
+}
 
-  function renderActivity (data){
+function renderActivity(data) {
     renderActivityImg(data)
     renderActivityText(data)
-  }
+}
 
- function renderActivityImg(data){
+function renderActivityImg(data) {
 
     let actImgAreacontent = ""
-    data.forEach(act=>{
-        actImgAreacontent+=
-        `
+    data.forEach(act => {
+        actImgAreacontent +=
+            `
         <img src="${act.img}" data-actid="${act.id}" alt="${act.title}">
         `
     })
     const actImgArea = document.querySelector(".js-act-imgs")
     actImgArea.innerHTML = actImgAreacontent
     observerActivityImg()
- }
+}
 
- function renderActivityText(data){
+function renderActivityText(data) {
     let actTextAreaContent = ""
-    data.forEach(act=>{
-        actTextAreaContent+=
-        `
+    data.forEach(act => {
+        actTextAreaContent +=
+            `
         <div class="item" data-actid=${act.id}>
         <div class="icon">
             <img src="img/paw.png" alt="">
@@ -51,55 +52,55 @@ function init() {
     })
     const actTextArea = document.querySelector(".js-act-text")
     actTextArea.innerHTML = actTextAreaContent
- }
+}
 
- init()
+init()
 
- function observerActivityImg() {
-    let option={
-        root:document.querySelector("js-act-imgs"),
-        threshold:1
+function observerActivityImg() {
+    let option = {
+        root: document.querySelector("js-act-imgs"),
+        threshold: 0.5
     }
-    const actImgObserver = new IntersectionObserver ((entries)=>{
-        entries.forEach(entry=>{
+    const actImgObserver = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
             const currentid = entry.target.dataset.actid
             const currentText = document.querySelector(`.js-act-text>.item[data-actid=${currentid}]`)
-            if(entry.isIntersecting){
+            if (entry.isIntersecting) {
                 currentText.classList.add("select")
-            }else if(!entry.isIntersecting){
+            } else if (!entry.isIntersecting) {
                 currentText.classList.remove("select")
             }
-            
+
         })
-     },option)
-    
-     const actImgs = document.querySelectorAll(".js-act-imgs>img")
-    actImgs.forEach(img=>{
+    }, option)
+
+    const actImgs = document.querySelectorAll(".js-act-imgs>img")
+    actImgs.forEach(img => {
         actImgObserver.observe(img)
     })
-   }
+}
 
- function renderPolicyText(data,id){
+function renderPolicyText(data, id) {
     const policyTextArea = document.querySelector(".js-policy-text-area")
-    const num = data.findIndex(i=>i.id===id)
-    const nowSelectedPolicy = data.filter(policy=>policy.id===id)[0]
-    let policyImgAreaContentUlContent=""
-    nowSelectedPolicy.content.forEach((i,index)=>{
-        policyImgAreaContentUlContent+=
-        `
+    const num = data.findIndex(i => i.id === id)
+    const nowSelectedPolicy = data.filter(policy => policy.id === id)[0]
+    let policyImgAreaContentUlContent = ""
+    nowSelectedPolicy.content.forEach((i, index) => {
+        policyImgAreaContentUlContent +=
+            `
         <li>
-                                <h5>0${index+1} ${i.title}</h5>
+                                <h5>0${index + 1} ${i.title}</h5>
                                 <p>${i.description}</p>
                             </li>
         `
-        
-    
+
+
     })
     let policyTextAreaContent = `
-    <div class="num">No.${num+1}</div>
+    <div class="num">No.${num + 1}</div>
     <div class="header">
     
-    <h3 class="policy-title">政策 No. 0<span class="no-id">.${num+1}</span></h3>
+    <h3 class="policy-title">政策 No. 0<span class="no-id">.${num + 1}</span></h3>
     <h4 class="subtitle">${nowSelectedPolicy.title}</h4>
 </div>
 <div class="policy-text-content">
@@ -107,67 +108,67 @@ function init() {
      ${policyImgAreaContentUlContent}   
     </ul>
 </div>`
-    
+
     policyTextArea.innerHTML = policyTextAreaContent
     policyTextArea.classList.add("display")
- }
+}
 
- function renderPolicy(data) {
-    let policyImgAreaContent="";
-    data.forEach(policy=>{
-        policyImgAreaContent+=
-        `
+function renderPolicy(data) {
+    let policyImgAreaContent = "";
+    data.forEach(policy => {
+        policyImgAreaContent +=
+            `
         <img src="${policy.img}" alt="${policy.title}" data-polid="${policy.id}">
         `
     })
     const policyImgArea = document.querySelector(".js-policy-img-area")
     policyImgArea.innerHTML = policyImgAreaContent
     observerPolicyImg(data)
-   }
+}
 
-function observerPolicyImg(data){
-    let option={
-        root:document.querySelector("js-policy-imgs"),
-        threshold:0.5
-    }    
-    const observer = new IntersectionObserver(entries=>{
-        entries.forEach(entry=>{
-            if(entry.isIntersecting){
+function observerPolicyImg(data) {
+    let option = {
+        root: document.querySelector("js-policy-imgs"),
+        threshold: 0.5
+    }
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
                 const nowId = entry.target.dataset.polid
-                renderPolicyText(data,nowId)
+                renderPolicyText(data, nowId)
                 entry.target.classList.add("select")
-            }else if(!entry.isIntersecting){
+            } else if (!entry.isIntersecting) {
                 entry.target.classList.remove("select")
             }
         })
-    },option)
+    }, option)
 
     const poliyImgs = document.querySelectorAll(".js-policy-img-area>img")
-    poliyImgs.forEach(img=>{
+    poliyImgs.forEach(img => {
         observer.observe(img)
     })
 }
 // 投票動畫
 const introBlock = document.querySelector(".vote-box-wrap")
 
-const introBlockObserver = new IntersectionObserver((entries)=>{
-    
-    const block=entries[0]
+const introBlockObserver = new IntersectionObserver((entries) => {
+
+    const block = entries[0]
     const ticket = document.querySelector("img.vote")
 
-    if(block.isIntersecting){
-        console.log("IN") 
-    ticket.style.transform="translateY(150%)"
-    }else if(!block.isIntersecting){
-        console.log("OUT")
-        ticket.style.transform="translateY(-10%)"
+    if (block.isIntersecting) {
+
+        ticket.style.transform = "translateY(150%)"
+    } else if (!block.isIntersecting) {
+
+        ticket.style.transform = "translateY(-10%)"
 
     }
     // console.log(blocks)
 
-},{
-    threshold:0,
-    rootMargin:"100px 0px -200px 0px"
+}, {
+    threshold: 0,
+    rootMargin: "100px 0px -200px 0px"
 
 })
 
@@ -175,11 +176,11 @@ introBlockObserver.observe(introBlock)
 
 // 渲染捐款方案
 
-function renderDonationProject(data){
+function renderDonationProject(data) {
     let donationProjectContent = ""
-    data.forEach(pro=>{
-        donationProjectContent+=
-        `<div class="donation-card">
+    data.forEach(pro => {
+        donationProjectContent +=
+            `<div class="donation-card">
         <div class="card-header">
             <h4 class="title">「 ${pro.title} 」</h4>
             <p class="amount">捐款新台幣${pro.amount}元</p>
@@ -200,36 +201,84 @@ function renderDonationProject(data){
 const donation = document.querySelector(".js-donation")
 
 
-donation.addEventListener("click",donate)
+donation.addEventListener("click", donate)
 
-function donate(e){
-    if(e.target.nodeName!=="BUTTON") return;
-    
-    const donateId  = e.target.dataset.donateId
-    let nowDonateAmount=0
+function donate(e) {
+    if (e.target.nodeName !== "BUTTON") return;
+
+    const donateId = e.target.dataset.donateId
+    let nowDonateAmount = 0
     let nowDonateName = ""
-    if(donateId==="custom"){
-        nowDonateName="自訂金額贊助"
+    if (donateId === "custom") {
+        nowDonateName = "自訂金額贊助"
         nowDonateAmount = document.querySelector(".js-custom-donate-amount").value;
-        document.querySelector(".js-donate-project-amount").textContent=nowDonateAmount
-        document.querySelector(".js-donate-project-name").textContent=nowDonateName
+        document.querySelector(".js-donate-project-amount").textContent = nowDonateAmount
+        document.querySelector(".js-donate-project-name").textContent = nowDonateName
         document.querySelector(".js-total-donate-amount").textContent = nowDonateAmount
-    }else{
+    } else {
         fetch("./data/data.json")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (result) {
-    const nowDonationProject = result.donation_project.filter(i=>i.id===donateId)[0]
-    nowDonateAmount= nowDonationProject.amount
-    nowDonateName = nowDonationProject.title
-    document.querySelector(".js-donate-project-amount").textContent=nowDonateAmount
-    document.querySelector(".js-donate-project-name").textContent=nowDonateName
-    document.querySelector(".js-total-donate-amount").textContent = nowDonateAmount
-  });
-  }
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (result) {
+                const nowDonationProject = result.donation_project.filter(i => i.id === donateId)[0]
+                nowDonateAmount = nowDonationProject.amount
+                nowDonateName = nowDonationProject.title
+                document.querySelector(".js-donate-project-amount").textContent = nowDonateAmount
+                document.querySelector(".js-donate-project-name").textContent = nowDonateName
+                document.querySelector(".js-total-donate-amount").textContent = nowDonateAmount
+            });
+    }
 
 
 
 
 }
+
+function renderSloganChip(data) {
+
+    const chipArea = document.querySelector(".chips")
+    let chips = ""
+    const transform = [{ "rotate": 2, "translateY":-600},
+    { "rotate": -10, "translateY":-550},
+    { "rotate": -20, "translateY":-500},
+    { "rotate": 7, "translateY":-550},
+    { "rotate": -10, "translateY":-500},
+    { "rotate": 15, "translateY":-480},
+    { "rotate": 5, "translateY":-500}]
+    for (let i=0;i<7;i++){
+        let colorIndex = (Math.floor(Math.random()*(data.color.length)))
+        let textIndex = (Math.floor(Math.random()*(data.content.length)))
+        const chip = 
+        `<div class="chip${i+1} chip" style="background: ${data.color[colorIndex]};transform: rotate(${transform[i].rotate}deg) translateY(${transform[i].translateY}px);">${data.content[textIndex]}</div>`
+        chips += chip
+    }
+    chipArea.innerHTML = chips
+
+    // console.log(document.querySelectorAll(".chip"))
+    bannerObserver.observe(bannerArea)
+}
+
+const bannerArea = document.querySelector(".banner")
+
+const bannerObserver = new IntersectionObserver((entries)=>{
+    const banner = entries[0]
+    const chipArea = document.querySelector(".chips")
+    const chips = document.querySelectorAll(".chip")
+    // console.log(chips)
+    
+   
+    
+    if(banner.isIntersecting){
+        chips.forEach((chip,index)=>{
+            const transformation = chip.style.transform
+            let start = transformation.indexOf("(")
+            let end = transformation.indexOf(")")
+            const deg = transformation.slice(start+1, end)
+            setTimeout(function(){
+                chip.style.transform=` rotate(${deg}) translateY(0)`
+            },index*50)
+        })
+    }},{
+    threshold:0.8
+})
